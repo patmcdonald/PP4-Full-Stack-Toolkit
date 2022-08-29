9389d889c31eb5d3219d8f11fac6be0de28b291c
Pat McDonald <patmcdonald@me.com>
2022-08-29 10:06:53 +0000

    settings.py add local url

--- PP4/settings.py
+++ PP4/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pp4pat.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['pp4pat.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
